74066c28b11846c1ab2450ebaf97a0495a95d76f
Avraham Luban <lubanavs@gmail.com>
2017-05-11 18:00:19 -0400

    added home pg

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,24 @@ class PagesController < ApplicationController
     render "home_page.html.erb"
   end
 
+  def add_a_program_method
+    render "add_a_program_page.html.erb"
+  end
+
+  def added_program_method
+    @program = params[:program]
+    @country = params[:country]
+    @state = params[:state]
+    @city = params[:city]
+    @resort = params[:resort]
+    @image = params[:image]
+    @site = params[:site]
+    Hotel.create(program: @program, country: @country, state: @state, city: @city, resort: @resort, image: @image, site: @site)
+    render "added_program_page.html.erb"
+  end
+
+  def programs_method
+    render "programs_page.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   get "/", to: "pages#home_page_method"
+  get "/add_a_program", to: "pages#add_a_program_method"
+  post "/added_program_result_page", to: "pages#added_program_method"
+  get "/programs", to: 'pages#programs_method'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
